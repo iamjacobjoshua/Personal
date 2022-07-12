08f51d7630eb2f18a98da7cfe0ac6e7dc388eb52
Joshua Jacob <officialjacobjay@gmail.com>
2022-07-12 22:32:59 +0100

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close');
+
+/* ========= SHOW MENU ========= */
+if (navToggle) {
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu');
+    })
+}
+
+/* ========= HIDE MENU ========= */
+if (navClose) {
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu');
+    })
+}
